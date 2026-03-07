7f03af4e480e53db8d3a10ffc048a1510adb81e5
0xgsvs <term@0xgsvs.dev>
2026-03-07 12:26:34 +0530

    feat: clone from anchor-bun-template

---
+++ programs/bun-kit/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+// pub use state::*;
+
+declare_id!("91fD8V2tNaVWcK8Mhk3xc6kzsnbevrFp77yLUtqn8DXA");
+
+#[program]
+pub mod bun_kit {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
